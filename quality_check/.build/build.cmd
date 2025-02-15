5bab5a2c624ec9a5f045db536e3bddc4b1fd0af3
os-strus <56672814+os-strus@users.noreply.github.com>
2025-02-15 08:43:28 +0100

    Migrated to Python 3.13

--- quality_check/.build/build.cmd
+++ quality_check/.build/build.cmd
@@ -21,7 +21,7 @@ if exist "translations" (
 echo Copying common files for zip
 xcopy ..\common\common_*.py . /Y > nul
 
-C:\tls\Python312\python ..\common\build.py
+C:\tls\Python313\python ..\common\build.py
 if %ERRORLEVEL% neq 0 goto :ExitPoint
 
 echo Deleting common files after zip
